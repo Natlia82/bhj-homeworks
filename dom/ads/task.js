5a5f2cd74f0980a66337b09d93e2dd650a818808
Natalia <nata@host>
2020-08-23 09:17:06 +0500

    Исправлено

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,12 +1,13 @@
 const block = document.getElementsByClassName("rotator__case");
-let n = document.getElementsByClassName("rotator__case").length;
+let n = document.getElementsByClassName("rotator__case").length - 1;
 let k = 0;
 
 function carousel() {
     block[k].classList.remove("rotator__case_active");
-    if (k == n) {
+    if (k === n) {
         k = 0;
     } else k++;
+
     block[k].classList.add("rotator__case_active");
 
 }
