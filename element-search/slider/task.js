94e121c09a127c4b6b52f3bb7c0a0a18c2e9a2f6
Natalia <nata@host>
2021-03-01 14:08:28 +0500

    ДЗ 2 готово

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -5,22 +5,53 @@ let picActive = document.getElementsByClassName("slider__item");
 let pictures = Array.from(picActive);
 
 next.onclick = function() {
-    let activ = ElemetActiv();
-    activ + 1 < picActive.length ? picActive[activ + 1].classList.add("slider__item_active") : picActive[0].classList.add("slider__item_active");
+    let activ = ElemetNoActiv();
+    if (activ + 1 < picActive.length) {
+        picActive[activ + 1].classList.add("slider__item_active");
+        ElementSlider(activ + 1);
+    } else {
+        picActive[0].classList.add("slider__item_active");
+        ElementSlider(0);
+    }
+
 
 }
 
 prev.onclick = function() {
-    let activ = ElemetActiv();
-    activ > 1 ? picActive[activ - 1].classList.add("slider__item_active") : picActive[picActive.length - 1].classList.add("slider__item_active");
+    let activ = ElemetNoActiv();
+    if (activ > 1) {
+        picActive[activ - 1].classList.add("slider__item_active");
+        ElementSlider(activ - 1);
+    } else {
+        picActive[picActive.length - 1].classList.add("slider__item_active");
+        ElementSlider(picActive.length - 1);
+    }
+
 
 }
 
-function ElemetActiv() {
+
+let sliderActiv = document.getElementsByClassName("slider__dot");
+sliderActiv[0].classList.add("slider__dot_active");
+let sliderMass = Array.from(sliderActiv);
+for (let i = 0; i < sliderActiv.length; i++) {
+    sliderActiv[i].onclick = function() {
+        ElemetNoActiv();
+        ElementSlider(i);
+        picActive[i].classList.add("slider__item_active")
+    }
+}
+
+function ElemetNoActiv() {
     console.log(picActive.length);
     let k = document.querySelector(".slider__item_active");
     let n = pictures.indexOf(k);
     k.classList.remove("slider__item_active");
     console.log(n);
     return n;
+}
+
+function ElementSlider(i) {
+    document.querySelector(".slider__dot_active") ? document.querySelector(".slider__dot_active").classList.remove("slider__dot_active") : "";
+    sliderActiv[i].classList.add("slider__dot_active");
 }
